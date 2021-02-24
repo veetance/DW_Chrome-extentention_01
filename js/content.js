d43c7e5d71deb619470f360a3829dc4f3d9040e9
veetance <divineokos@hotmail.com>
2021-02-24 00:10:44 -0700

    Chrome extention initial commit

---
+++ js/content.js
@@ -0,0 +1,7 @@
+console.log("extension is running!")
+
+// TRY: updating the text content of an element or set of elements on one or more pages
+
+// TRY: adding a click event listener to an element that changes the background of that element to a new color
+
+// TRY: using a mouseenter event listener to update text to something new.
---
+++ js/p5.js
@@ -0,0 +1,6 @@
+function setup() {
+createCanvas(400,400);
+
+
+
+}
